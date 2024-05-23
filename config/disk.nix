5124ef6bb8f850cc468ad203cf0ce6267acdb3eb
Austin Nason <austin.nason@schrodinger.com>
2024-05-22 18:29:06 -0700

    disk device

--- config/disk.nix
+++ config/disk.nix
@@ -4,7 +4,7 @@ _: {
   disko.devices = {
     disk = {
       sda = {
-        device = "/dev/%DISK%";
+        device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
